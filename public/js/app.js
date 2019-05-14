619025bb35964f72a7d7bbfebd7c2021375400cd
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-05-14 14:56:17 +0100

    Fine tune client-side js & update form styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,23 @@
 const form = document.querySelector("form");
 const search = document.querySelector("input");
+const msg1 = document.querySelector("#msg-1");
+const msg2 = document.querySelector("#msg-2");
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log('Fetching weather...');
+    msg1.textContent = 'Fetching weather...';
+    msg2.textContent = '';
 
     const location = search.value;
     fetch(`http://localhost:3000/weather?address=${location}`).then(res => {
         res.json().then(data => {
             if (data.error) {
                 console.log(data.error);
+                msg1.textContent = 'Error:';
+                msg2.textContent = data.error;
             } else {
                 console.log(data);
+                msg1.textContent = `Location: ${data.location}, Temperature: ${data.temperature} degrees, Precipitation %: ${data.precipProbability}%, Summary: ${data.summary}`;
             }
         });
     });
